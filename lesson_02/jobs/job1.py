36a1dbc5d33f7851a60ff72ed864441d5e43a991
ihor.kpl <lov3catch@gmail.com>
2024-04-10 20:50:24 +0300

    - recur

--- lesson_02/jobs/job1.py
+++ lesson_02/jobs/job1.py
@@ -1,10 +1,14 @@
+import json
 import os
 import shutil
 
 import requests
 
 
-def main(data, page, token):
+def main(data, token, page=1, acc=None) -> str:
+    if acc is None:
+        acc = []
+
     response = requests.get(
         url='https://fake-api-vycpfa6oca-uc.a.run.app/sales',
         params={'date': data, 'page': page},
@@ -12,26 +16,29 @@ def main(data, page, token):
     )
 
     if 200 == response.status_code:
-        return response.text
+        acc = acc + response.json()
+        return main(data, token, page + 1, acc)
+
+    if 404 == response.status_code:
+        return json.dumps(acc)
 
     raise Exception('Can not parse API')
 
 
-def save(payload, base_path, data, page):
+def save(payload, base_path, data):
     shutil.rmtree(base_path)
     os.makedirs(base_path, exist_ok=True)
-    path = f'{base_path}/sales-{data}-{page}.json'
+    path = f'{base_path}/sales-{data}.json'
     with open(path, 'w+') as f:
         f.write(payload)
 
 
 if __name__ == '__main__':
     data = '2022-08-09'
-    page = 2
 
     AUTH_TOKEN = os.environ['AUTH_TOKEN']
-    payload = main(data, page, AUTH_TOKEN)
+    payload = main(data, AUTH_TOKEN)
 
     base_path = f'/app/file_storage/raw/sales/{data}'
 
-    save(payload, base_path, data, page)
+    save(payload, base_path, data)
