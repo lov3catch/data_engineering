4cb94936bfdf7dedcc625105766884a26e23a72e
ihor.kpl <lov3catch@gmail.com>
2024-04-15 15:01:14 +0300

    - codestyle

--- lesson_03/home_task_queries.sql
+++ lesson_03/home_task_queries.sql
@@ -31,13 +31,15 @@ WITH TopActors AS (SELECT film_actor.actor_id,
                    GROUP BY film_actor.actor_id
                    ORDER BY rental_count DESC
                    LIMIT 10)
+-- Select the top actors' details from the 'actor' table by joining it with the 'TopActors' CTE.
 SELECT TopActors.actor_id,
        actor.first_name,
        actor.last_name,
        TopActors.rental_count
 FROM TopActors
-         JOIN actor ON actor.actor_id = TopActors.actor_id
-ORDER BY rental_count desc;
+         JOIN actor ON actor.actor_id = TopActors.actor_id -- Join 'actor' table on actor ID to get actor details
+ORDER BY rental_count DESC;
+
 
 
 
@@ -89,21 +91,16 @@ WHERE i.inventory_id IS NULL;
 Вивести топ 3 актори, які найбільше зʼявлялись в категорії фільмів “Children”.
 */
 -- Selects top 3 actors who appeared most frequently in films categorized as 'Children'
-SELECT
-    a.first_name,
-    a.last_name,
-    COUNT(fa.actor_id) AS appearance_count
-FROM
-    film_actor fa
-    LEFT JOIN actor a ON fa.actor_id = a.actor_id
-    JOIN film_category fc ON fa.film_id = fc.film_id
-    JOIN category c ON c.category_id = fc.category_id
-WHERE
-    c.name = 'Children'
-GROUP BY
-    fa.actor_id,
-    a.first_name,
-    a.last_name
-ORDER BY
-    appearance_count DESC
+SELECT a.first_name,
+       a.last_name,
+       COUNT(fa.actor_id) AS appearance_count
+FROM film_actor fa
+         LEFT JOIN actor a ON fa.actor_id = a.actor_id
+         JOIN film_category fc ON fa.film_id = fc.film_id
+         JOIN category c ON c.category_id = fc.category_id
+WHERE c.name = 'Children'
+GROUP BY fa.actor_id,
+         a.first_name,
+         a.last_name
+ORDER BY appearance_count DESC
 LIMIT 3;
