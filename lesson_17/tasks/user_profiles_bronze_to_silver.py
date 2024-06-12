ba21d485f43519cc57cedbdf280685a0d496a8d5
ihor.kpl <lov3catch@gmail.com>
2024-06-12 14:27:55 +0300

    - add age column

--- lesson_17/tasks/user_profiles_bronze_to_silver.py
+++ lesson_17/tasks/user_profiles_bronze_to_silver.py
@@ -20,6 +20,7 @@ user_profiles_df = spark.read.json('/app/file_storage/processed/bronze/user_prof
 split_col = F.split(user_profiles_df['full_name'], ' ')
 user_profiles_df = user_profiles_df.withColumn('first_name', split_col.getItem(0))
 user_profiles_df = user_profiles_df.withColumn('last_name', split_col.getItem(1))
+user_profiles_df = user_profiles_df.withColumn("age",F.floor(F.datediff(F.current_date(), F.col("birth_date")) / 365.25))
 
-user_profiles_df = user_profiles_df.select('email', 'first_name', 'last_name', 'state', 'birth_date', 'phone_number')
+user_profiles_df = user_profiles_df.select('email', 'first_name', 'last_name', 'state', 'birth_date', 'age', 'phone_number')
 user_profiles_df.write.csv('/app/file_storage/processed/silver/user_profiles/user_profiles.csv', header=True)
